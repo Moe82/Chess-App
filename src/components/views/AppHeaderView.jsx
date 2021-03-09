5d0f75d8817045702574a45d72be1e756e48a9df
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-09 16:18:14 -0500

    change app title

--- src/components/views/AppHeaderView.jsx
+++ src/components/views/AppHeaderView.jsx
@@ -6,7 +6,7 @@ function AppHeaderView() {
   return (
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 style={{"marginTop": "-15px", "color": "white", "marginBottom": "-15px"}}>React Chess</h1>
+      <h1 style={{"marginTop": "-15px", "color": "white", "marginBottom": "-15px"}}>A React Chess App</h1>
       <big style={{"color": "white", "display": "inline-block"}}>Written by</big> <a className="App-link" href="https://github.com/Moe82">@Moe82</a>
     </div>
   )
