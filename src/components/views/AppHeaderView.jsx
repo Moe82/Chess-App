04085f8d97bdab7fc965f9a73d2707a92c7cc558
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-09 15:02:51 -0500

    create app header

---
+++ src/components/views/AppHeaderView.jsx
@@ -0,0 +1,15 @@
+
+import logo from '../../logo.svg';
+import '../../App.css';
+
+function AppHeaderView() {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 style={{"marginTop": "-15px", "color": "white", "marginBottom": "-15px"}}>React Chess</h1>
+      <big style={{"color": "white", "display": "inline-block"}}>Written by</big> <a className="App-link" href="https://github.com/Moe82">@Moe82</a>
+    </div>
+  )
+}
+
+export default AppHeaderView;
