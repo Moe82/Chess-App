9b22a921d412895abef8844e71b4d5edcecd0ac2
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-09 16:21:57 -0500

    add alert box for unimplemented feature

--- src/components/views/GameSelectView.jsx
+++ src/components/views/GameSelectView.jsx
@@ -3,6 +3,7 @@ import { nanoid } from 'nanoid'
 import Button from 'react-bootstrap/Button';
 
 function GameSelectView(props) {  
+  
   const handleFriendBtnClick = () => {
     let colors = ["white","black"];
     let userColor = colors[Math.floor(Math.random()*colors.length)];
@@ -11,10 +12,14 @@ function GameSelectView(props) {
     props.history.push(userUrl)
   }
 
+  const handleAIBtnClick = () => {
+    window.alert("Sorry, this feature is not yet ready! Try playing with a friend.")
+  }
+
   return (
     <div style={{"textAlign": "center"}}>
       <Button className="App-btn" variant="primary" onClick={handleFriendBtnClick}> Play a Friend </Button>{' '} <br />
-      <Button className="App-btn" variant="primary" size="big"> Battle The AI </Button>{' '} 
+      <Button className="App-btn" variant="primary" size="big" onClick={handleAIBtnClick}> Battle The AI </Button>{' '} 
     </div>
   )
 }
