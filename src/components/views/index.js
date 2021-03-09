8c8535ac9300170842495f9006b1fb7074b2d956
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-09 14:47:46 -0500

    add exports for GameSelectView and AppHeaderView

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,2 +1,3 @@
 export { default as ChessboardView } from './ChessboardView'
 export { default as GameSelectView } from './GameSelectView'
+export { default as AppHeaderView } from './AppHeaderView'
