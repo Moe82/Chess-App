c7bb64f5d18cc2c0732e035c714e5f990b209f26
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-10 12:52:09 -0500

    style the chessboard to match theme

--- src/components/views/ChessboardView.jsx
+++ src/components/views/ChessboardView.jsx
@@ -12,9 +12,10 @@ function ChessboardView(props) {
       onDrop={(move) => props.handleMove(move)}
       orientation={props.color}
       dropOffBoard={'snapback'}
-      appearSpeed={'slow'}
-      moveSpeed={99999}
       orientation={props.color}
+      dropSquareStyle={{ boxShadow: 'inset 0 0 1px 4px black' }}
+      lightSquareStyle={{ backgroundColor: '#61dafb' }}
+      darkSquareStyle={{ backgroundColor: '#6c99bb' }}
     />
     </div>  
   )
