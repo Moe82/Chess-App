aad302ed93df345e29da73100d1750e014da216c
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-10 16:36:34 -0500

    use history instead of history/createBrowserHistory to fix warning

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
-import CreateHistory from  "history/createBrowserHistory";
+import history from "./history";
 
 ReactDOM.render(
-  <Router history = {CreateHistory()}>
+  <Router history = {history}>
     <App />
   </Router>,
   document.getElementById('root')
