e614fbb6f35c020678e34242e7252da40fb1e096
Mohamed <Moe@iMac-2.local>
2025-07-28 16:33:32 -0400

    update backend url

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
-let socket = io("https://chess-app-backend.herokuapp.com");
+let socket = io("https://chess-app-backend-345d2453b5cc.herokuapp.com/");
 export default socket;
