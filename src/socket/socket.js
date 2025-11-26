eb10973a2eaade45bef91ef5ca6e3afedbf439f0
Mohamed Shafee <mshafee1@outlook.com>
2025-11-26 13:42:19 -0500

    Update backend url

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
-let socket = io("https://chess-app-backend-345d2453b5cc.herokuapp.com/");
+let socket = io("https://chess-app-backend-production.up.railway.app");
 export default socket;
